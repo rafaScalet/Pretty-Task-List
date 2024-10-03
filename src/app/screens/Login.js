6e51313a1dc9054603f9e2d5720295929cbfb5d0
rafaScalet <rafaelscalet2018@gmail.com>
2024-10-03 09:46:13 -0300

    refactor

--- src/app/screens/Login.js
+++ src/app/screens/Login.js
@@ -9,7 +9,7 @@ export function Login() {
 
   return (
     <SafeAreaView style={styles.container}>
-			<Image source={require('./../../Assets/logo.svg')} />
+			<Image source={require('./../Assets/logo.svg')} />
       <Text style={styles.title}>TaskList</Text>
       <TextInput style={styles.input} placeholder="Enter your email" onChangeText={(inputEmail) => setEmail(inputEmail)} value={email}/>
       <TextInput style={styles.input} placeholder="Enter your password" onChangeText={(inputPassword) => setPassword(inputPassword)} value={password} secureTextEntry={true}/>
